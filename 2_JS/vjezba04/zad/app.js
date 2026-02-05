48f06dedc3252728336169e77ba4e7e97ff76136
Ivica Šamu <ivica.samu@gmail.com>
2026-02-05 18:56:32 +0100

    Vjezba 04 DZ

--- 2_JS/vjezba04/zad/app.js
+++ 2_JS/vjezba04/zad/app.js
@@ -8,7 +8,135 @@ document.getElementById('izvedi').addEventListener('click', () => {
   const d = document.getElementById('d').value;
   const zadatak = document.getElementById('zadatak').value;
 
-  //4. zadatak
+  // 4. zadatak
+  //    Provjera punoljetnosti: 
+  //  Korisnik unosi godinu rođenja u polje A. 
+  //  Izračunaj njegovu dob u odnosu na tekuću godinu i ispiši je li punoljetan ili nije.
+  if(zadatak === '4'){
+    const trenutnaGodina = 2026
+    const x = Number(a);
+    if(!x){
+      rezultat.innerHTML='A nije broj';
+      return;
+    }
+
+    ukupnoGodina = trenutnaGodina - x
+
+    if(ukupnoGodina >= 18){
+      rezultat.innerHTML = 'Osoba je punoljetna'
+    } else {
+      rezultat.innerHTML = 'Osoba je maloljetna'
+    }
+
+    return;
+  }
+  /*
+  [ZADATAK 5]
+ Pozitivan, negativan ili nula: 
+ Za uneseni broj u polju A ispiši poruku je li on pozitivan, negativan ili je nula.
+ ----------------------------------------------------------------*/
+
+  if(zadatak === '5'){
+    const x = Number(a);
+    if(!x && x!=0){
+      rezultat.innerHTML='A nije broj';
+      return;
+    }
+
+    if(x > 0){
+      rezultat.innerHTML='A je pozitivan';
+    } else if(x < 0){
+      rezultat.innerHTML='A je negativan';
+    } else {
+      rezultat.innerHTML='A je 0';
+    }
+
+    return
+  }
+
+ /*[ZADATAK 6]
+ Parnost broja: 
+ Provjeri je li uneseni broj u polju A paran ili neparan koristeći modulo operator (%).
+
+ ----------------------------------------------------------------*/
+ 
+   if(zadatak === '6'){
+    const x = Number(a);
+    if(!x){
+      rezultat.innerHTML='A nije broj';
+      return;
+    }
+    if(x%2 === 0){
+      rezultat.innerHTML = 'A je paran broj';
+    } else {
+      rezultat.innerHTML = 'A je neparan broj';
+    }
+
+
+    return
+  }
+
+ /*[ZADATAK 7]
+ Ocjenjivanje: 
+ Na temelju unesenog broja bodova (0-100) u polje A, ispiši ocjenu:
+ 90-100: Odličan, 80-89: Vrlo dobar, 70-79: Dobar, 60-69: Dovoljan, <60: Nedovoljan.
+
+ ----------------------------------------------------------------*/
+
+   if(zadatak === '7'){
+    const x = Number(a);
+    if(!x){
+      rezultat.innerHTML='A nije broj';
+      return;
+    }
+
+    if(x>0 && x<100){
+      if(x<60){
+        rezultat.innerHTML='Nedovoljan';
+      }else if(x<70){
+        rezultat.innerHTML='Dovoljan';
+      }else if(x<80){
+        rezultat.innerHTML='Dobar';
+      }else if(x<90){
+        rezultat.innerHTML='Vrlo dobar';
+      }else if(x<100){
+        rezultat.innerHTML='Odličan';
+      }
+    }else{
+      rezultat.innerHTML='Broj bodova mora biti 0-100';
+    }
+
+    return
+  }
+ /*[ZADATAK 8]
+ Djeljivost: 
+ Provjeri je li broj u polju A djeljiv s brojem u polju B bez ostatka. 
+ Ispiši "Djeljiv je" ili "Nije djeljiv".  */
+
+   if(zadatak === '8'){
+    const x = Number(a);
+    if(!x){
+      rezultat.innerHTML='B nije broj';
+      return;
+    }
+
+    const y = Number(a);
+    if(!y){
+      rezultat.innerHTML='B nije broj';
+      return;
+    }
+
+    rezDjeljanja = a%b
+    if(rezDjeljanja === 0){
+      rezultat.innerHTML='Djeljiv je';
+    } else{
+      rezultat.innerHTML='Nije djeljiv';
+    }
+    
+
+
+    return
+  }
 
   if(zadatak === '3'){
     const x = Number(a);
