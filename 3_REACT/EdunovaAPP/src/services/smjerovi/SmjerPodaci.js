63bd3e339f866497339097f202a47a32f911cd53
Ivica Šamu <ivica.samu@gmail.com>
2026-03-18 13:53:45 +0100

    Servis za smjer i osnovni prikaz podataka

---
+++ 3_REACT/EdunovaAPP/src/services/smjerovi/SmjerPodaci.js
@@ -0,0 +1,18 @@
+export const smjerovi = [
+    {
+        sifra: 1,
+        naziv: 'web programiranje',
+        trajanje: 130,
+        cijena: 1250.99,
+        datumPokretanja: '2024-02-12T17:00:00',
+        aktivan: true
+    },
+    {
+        sifra: 2,
+        naziv: 'java programiranje',
+        trajanje: 130,
+        cijena: 1500,
+        datumPokretanja: '2019-02-12T17:00:00',
+        aktivan: false
+    }
+]
